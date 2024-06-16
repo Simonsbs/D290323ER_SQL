42a1c8ef9ed3b66a8c2b8bac316c9da073d03f12
Simon B.Stirling <simonsbs@gmail.com>
2024-06-16 20:01:41 +0300

    joins

---
+++ SQLQuery4.sql
@@ -0,0 +1,54 @@
+/*
+SELECT
+	*
+FROM
+	[Categories]
+
+SELECT
+	*
+FROM
+	[Products]
+
+
+SELECT
+	*
+FROM
+	[Products] INNER JOIN [Categories]
+		ON [Products].[CategoryID] = [Categories].[ID]
+
+
+*/
+
+
+USE [NewDB];
+
+SELECT
+	*
+FROM
+	[Users] INNER JOIN [Departments]
+		ON [Users].[DepartmentID] = [Departments].[ID]
+
+SELECT
+	*
+FROM
+	[Users] LEFT JOIN [Departments]
+		ON [Users].[DepartmentID] = [Departments].[ID]
+
+SELECT
+	*
+FROM
+	[Users] RIGHT JOIN [Departments]
+		ON [Users].[DepartmentID] = [Departments].[ID]
+
+--SELECT * FROM [Departments]
+--SELECT * FROM [Users] WHERE [Users].[DepartmentID] = 1
+--SELECT * FROM [Users] WHERE [Users].[DepartmentID] = 2
+--SELECT * FROM [Users] WHERE [Users].[DepartmentID] = 3
+--SELECT * FROM [Users] WHERE [Users].[DepartmentID] = 4
+--SELECT * FROM [Users] WHERE [Users].[DepartmentID] = 10
+
+SELECT
+	*
+FROM
+	[Users] FULL JOIN [Departments]
+		ON [Users].[DepartmentID] = [Departments].[ID]
