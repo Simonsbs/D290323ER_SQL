8c94c19c4fee22634e9963ed4ecc7ae82a608eea
Simon B.Stirling <simonsbs@gmail.com>
2024-06-23 18:49:27 +0300

    Functions

---
+++ Functions.sql
@@ -0,0 +1,32 @@
+ALTER FUNCTION GetTotalSales (
+	@EmployeeID INT
+)RETURNS MONEY
+AS
+BEGIN
+	DECLARE @Result MONEY;
+
+	SELECT
+		@Result = SUM([Freight])
+	FROM
+		[Orders]
+	WHERE
+		[EmployeeID] = @EmployeeID
+
+	RETURN @Result;
+END
+
+GO
+
+SELECT
+	[EmployeeID],
+	dbo.GetTotalSales([EmployeeID]) AS [TotalSales],
+	[FirstName] + ' '+ [LastName] AS [Name]
+FROM
+	[Employees]
+
+
+/*
+
+Create a function to calculate the total order amount including a discount
+
+*/
