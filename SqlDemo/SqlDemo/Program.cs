db6eac07431728ebeb486678eed7488016765c22
Simon B.Stirling <simonsbs@gmail.com>
2024-06-23 20:27:44 +0300

    Sql C# Demo

---
+++ SqlDemo/SqlDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+internal class Program
+{
+	private static void Main(string[] args)	{
+		string connectionString = "Server=TEST_ENV\\SQL2022;Database=NW2;Trusted_Connection=True;TrustServerCertificate=true;";
+
+		SqlConnection connection = new SqlConnection(connectionString);
+
+		connection.Open();
+
+		SqlCommand cmd = new SqlCommand("SELECT [CustomerID], [City], [CompanyName] FROM [Customers];", connection);
+
+		SqlDataReader reader = cmd.ExecuteReader();
+
+		while (reader.Read()) {
+			Console.WriteLine(reader["CompanyName"]);
+        }
+	}
+}
