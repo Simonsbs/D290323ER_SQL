4fa276f448fcc2907d5602750aa609284f7dc617
Simon B.Stirling <simonsbs@gmail.com>
2024-06-23 20:00:20 +0300

    .

--- Functions.sql
+++ Functions.sql
@@ -1,4 +1,4 @@
-ALTER FUNCTION GetTotalSales (
+/*ALTER FUNCTION GetTotalSales (
 	@EmployeeID INT
 )RETURNS MONEY
 AS
@@ -24,10 +24,95 @@ SELECT
 FROM
 	[Employees]
 
-
+	*/
 /*
 
 Create a function to calculate the total order value 
 amount including a discount in a specific category
 
 */
+
+/*
+ALTER FUNCTION GetTotalOrderValueByCategory(
+	@CategoryID INT
+) RETURNS MONEY
+AS
+BEGIN
+
+	DECLARE @Result MONEY
+
+	SELECT
+		@Result = SUM(([OD].[UnitPrice] * [OD].[Quantity]) * (1 - [OD].[Discount])) 
+	FROM
+		[Order Details] AS [OD]
+		INNER JOIN
+		[Products] AS [P]
+			ON [OD].[ProductID] = [P].[ProductID]
+	WHERE
+		[P].[CategoryID] = @CategoryID
+
+	RETURN @Result
+
+END
+
+GO
+
+SELECT
+	dbo.GetTotalOrderValueByCategory([CategoryID]) AS [Total]
+FROM
+	[Categories]
+
+	*/
+	/*
+GO
+
+ALTER FUNCTION GetCustomerOrdersTbl (
+	@CustomerID NVARCHAR(5)
+)RETURNS TABLE
+AS
+RETURN (
+	SELECT
+		*
+	FROM
+		[Orders]
+	WHERE
+		[CustomerID] = @CustomerID
+)
+
+GO
+
+SELECT
+	*
+FROM
+	dbo.GetCustomerOrdersTbl('BLAUS')
+
+	*/
+
+/*
+
+Create a table-valued function to return the
+names and prices of products in a specific category.
+
+*/
+
+ALTER FUNCTION GetProductNamesByCategory (
+	@CategoryID INT
+)RETURNS TABLE
+AS
+RETURN (
+	SELECT
+		[ProductName],
+		[UnitPrice]
+	FROM
+		[Products]
+	WHERE
+		[CategoryID] = @CategoryID
+)
+
+
+GO
+
+SELECT
+	*
+FROM
+	dbo.GetProductNamesByCategory(2)
